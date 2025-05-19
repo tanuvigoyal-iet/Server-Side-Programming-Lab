1408e2cb8485939b4a865bec868b23c35ddf9729
tanuvigoyal-iet <22bcs070@ietdavv.edu.in>
2025-05-19 12:39:18 +0530

    Add files via upload

---
+++ Assignment -5.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/protected/*")
+public class AuthenticationFilter implements Filter {
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        boolean isLoggedIn = (session != null && session.getAttribute("username") != null);
+
+        if (isLoggedIn) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.html");
+        }
+    }
+
+    public void destroy() {
+        
+    }
+}
